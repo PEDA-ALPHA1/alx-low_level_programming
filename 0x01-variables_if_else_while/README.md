9125975c5e7666707565b7967a7a244062ea1ece
PEDA-ALPHA1 <daep100@gmail.com>
2021-06-20 11:08:33 -0700

    Using if and else statements

---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+**Mandatory for all ALX School Project**
---
+++ test.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+int main (void)
+{
+  int a = 21;
+  int b = 20;
+  int c ;
+
+  c = a + b;
+  printf("Line 1 - Value of c is %\n", c);
+
+  c = a - b;
+  printf("Line 2 - Value of c is %\n", c);
+
+  c = a * b;
+  printf("Line 3 - Value of c is %\n", c);
+
+  c = a++;
+  printf("Line 4 - Value of c is %\n", c);
+
+c = b--;
+printf("Line 5 - Value of c is %\n", c);
+return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	 
