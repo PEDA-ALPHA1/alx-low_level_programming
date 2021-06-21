74155edbee9b67c14851644c32fd3e82de795578
PEDA-ALPHA1 <daep100@gmail.com>
2021-06-21 12:53:55 -0700

    Print alphabets backward

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,7 +3,8 @@
 #include <time.h>
 /**
 * main - main block
-* Description: Get a random number and print the number * and if it is positive, negative, or zero 
+* Description: Get a random number and print the number
+* and if it is positive, negative, or zero
 * Return: 0
 */
 int main(void)
