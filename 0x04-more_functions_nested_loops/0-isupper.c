40e949ddbd2745e278c249b5d37d0f23de82de7f
PEDA-ALPHA1 <daep100@gmail.com>
2021-06-24 12:32:48 -0700

    ASCII functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * checks for uppercase character.
+ * use prototype: int _isupper(int c);
+ * return 1 if c is uppercase
+ * return 0 otherwise
+ */
+int _isupper(int x)
+{
+  if(x >= 65 && x <=90)
+    {
+      return (1);
+    }else
+    {
+      return (0);
+    }
+}
