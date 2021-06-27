c3aad5f5b7c5275f546fe6e1e44a4594418babc0
PEDA-ALPHA1 <daep100@gmail.com>
2021-06-26 17:28:36 -0700

    Print lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/* function that prints alphabets in lowercase
+ * use prototype void print_alphabet(void)
+ * only use _putchar
+ * Return: 0
+ */
+int main(void)
+{
+_putchar x;
+for (x = '97';x <= '122';x++)
+{_putchar (x);
+}
+_putchar ('\n');
+return (0);
+}
