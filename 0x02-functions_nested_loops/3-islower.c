18961ac58694295bf42d3b0a81c5994d78627c2d
PEDA-ALPHA1 <daep100@gmail.com>
2021-06-27 07:05:48 -0700

    Check for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* int _islower(int c) - check for lowercase character
+* Return: 1 if c is lower
+* Return: 0 otherwise
+*/
+int _islower(int c)
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+return (1);
+}
+else
+{
+return (0)
+}
+}
