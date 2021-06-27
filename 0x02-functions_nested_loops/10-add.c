ad615dd61925f0836b3bee30fd332b37cae53569
PEDA-ALPHA1 <daep100@gmail.com>
2021-06-27 08:40:12 -0700

    Print sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+* add - Add two numbers
+* @x: Number x
+* @y: Number y
+* Return: Return the sum
+*/
+int add(int x, int y)
+{
+int sum;
+sum = x + y;
+return (sum);
+}
