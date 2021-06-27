99cece4bbf9afda6a29f8e7847e38b40161fe88b
PEDA-ALPHA1 <daep100@gmail.com>
2021-06-26 19:25:42 -0700

    Print lowwercase x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
 * main - Entry point
-* function that prints x10 the alphabets in lowercase
-*Return: Always void
+* print_alphabet_x10 - function that prints 10 times the alphabet, in lowercase, followed by a new line.
+* Return: Always void
 */
 void print_alphabet_x10(void)
 {
 int i;
 int j;
- for (i = 1; i <= 10; i++)
+for(i = 1; i <= 10; i++)
 {
 for (j = 97; j <= 122; j++)
 {
