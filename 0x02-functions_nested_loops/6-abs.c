4e23128451c9a83b47f4530d50a096dbdae87359
PEDA-ALPHA1 <daep100@gmail.com>
2021-06-27 07:52:05 -0700

    Compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* _abs - Print alphabet in lowercase 10 times
+* Return: 0
+* @r: char type letter
+*/
+int _abs(int r)
+{
+if (r >= 0)
+{
+return (r);
+}
+else
+{
+return (r * -1);
+}
+}
