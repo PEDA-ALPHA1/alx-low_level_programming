1a88b15091391d6df51de84f2a60fb8dabfbdd2a
PEDA-ALPHA1 <daep100@gmail.com>
2021-06-27 07:47:39 -0700

    Print number sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* print_sign - Print alphabet in lowercase 10 times
+* Return: 0
+* @n: char type letter
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar ('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar ('-');
+return (-1);
+}
+else
+{
+_putchar ('0');
+return (0);
+}
+}
