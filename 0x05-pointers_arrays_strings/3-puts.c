3bbf3f9e8acb8dc69ba1a339ecefe1d79d852416
PEDA-ALPHA1 <daep100@gmail.com>
2021-06-29 12:49:54 -0700

    Print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* _puts - Print the last digit
+* @str: Number that is going to be splited
+* Return: Return the last digit
+*/
+void _puts(char *str)
+{
+int c = 0;
+while (str[c] != '\0')
+{
+_putchar (str[c]);
+c++;
+}
+_putchar ('\n');
+}
