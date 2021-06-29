de77c05e032c1815e32db8e3b65dd77172cfb856
PEDA-ALPHA1 <daep100@gmail.com>
2021-06-29 12:14:46 -0700

    Take pointer to int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+* reset_to_98 - Print the last digit
+* @n: Number that is going to be splited
+* Return: Return the last digit
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
