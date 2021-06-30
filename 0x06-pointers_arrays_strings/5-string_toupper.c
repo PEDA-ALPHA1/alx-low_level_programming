d24a26c97c60cfc626ad594d2aa179b5623b6eb1
PEDA-ALPHA1 <daep100@gmail.com>
2021-06-30 04:03:46 -0700

    Lowercase to UPPERCASE

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* string_toupper - Change all lowercase letters to uppercase
+* @c: string
+* Return: c
+*/
+char *string_toupper(char *c)
+{
+int x;
+for (x = 0; c[x] != '\0'; x++)
+{
+if (c[x] >= 97 && c[x] < 123)
+{
+c[x] = c[x] - 32;
+}
+}
+return (c);
+}
