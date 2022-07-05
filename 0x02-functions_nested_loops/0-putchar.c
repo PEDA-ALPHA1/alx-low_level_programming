600b346c41189676ea1dfc64ee7d0a9d0b491588
Peda-Alpha <peda4sure@gmail.com>
2022-07-05 09:52:20 -0700

    Print _putchar using a function prototype

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - prints putchar followed by a newline
+* Return: 0
+*/
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
