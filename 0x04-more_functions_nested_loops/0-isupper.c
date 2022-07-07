c55bef8559fa34f833bef410fc1a80a62749ff02
Peda-Alpha <peda4sure@gmail.com>
2022-07-07 15:55:18 -0700

    Check for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * checks for uppercase character.
  * use prototype: int _isupper(int c);
@@ -7,11 +7,12 @@
  */
 int _isupper(int x)
 {
-  if(x >= 65 && x <=90)
-    {
-      return (1);
-    }else
-    {
-      return (0);
-    }
+	if (x >= 65 && x <=90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
