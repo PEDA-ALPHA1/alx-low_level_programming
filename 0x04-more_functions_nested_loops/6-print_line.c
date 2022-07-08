69bf43db2293c58af5370ffd26ebc51234728e30
Peda-Alpha <peda4sure@gmail.com>
2022-07-07 18:58:51 -0700

    Draw a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_line - Function prototype
+ * @n: int type character
+ * Description: Function that draws a straight line in the terminal
+ * Return: Always 0
+ */
+void print_line(int n);
+
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= n; x++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
