73839ac731d5764c6a4fb42e7475a65612ac8bfa
Peda-Alpha <peda4sure@gmail.com>
2022-07-20 09:37:27 -0700

    Print string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev_recursion - Function prototype
+ * Description: Print a string in reverse
+ * Followed by a new line
+ * @s: Pointer variable of type char
+ * Return: void
+ * **/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar ('\n');
+	}
+	return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
