95a3b1f9b5d1d87e6407e8bb873c3df1623610d7
Peda-Alpha <peda4sure@gmail.com>
2022-07-22 07:59:20 -0700

    Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: Program that prints its name Followed by a new line
+ *If program is renamed, it will print the new name
+ * Without having to compile again
+ * Also, do not remove the path before the name of the program
+ * Return: Always 0
+ * **/
+int main (int argc, char *argv[])
+{
+	if (argc > 0)
+	printf("%s\n", argv[0]);
+	return (0);
+}
