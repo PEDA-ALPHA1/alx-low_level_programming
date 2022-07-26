74315660045478fc75da4496cc21c687b30166e9
Peda-Alpha <peda4sure@gmail.com>
2022-07-26 05:45:01 -0700

    Use the static library to print text

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+int _strlen(char *s);
+/**
+* *_strcat - Print the last digit
+* @dest: Number that is going to be splited
+* @src: Number that is going to be splited
+* Return: Return the last digit
+*/
+char *_strcat(char *dest, char *src)
+{
+int size1 = _strlen(dest);
+int size2 = _strlen(src);
+int x;
+int y = 0;
+for (x = size1; x < (size1 + size2); x++)
+{
+dest[x] = src[y];
+y++;
+}
+dest[x] = '\0';
+return (dest);
+}
+/**
+* _strlen - swap funcion
+* @s: Number that is going to be splited
+* Return: Return the last digit
+*/
+int _strlen(char *s)
+{
+int c = 0;
+while (s[c] != '\0')
+{
+c++;
+}
+return (c);
+}
