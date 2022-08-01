7c9e3b7879a90a563908a6d17e4dd738ab615116
Peda-Alpha <peda4sure@gmail.com>
2022-08-01 16:56:35 -0700

    Define a new type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - Data type
+ * Description: Contain dog's details
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * **/
 struct dog
 {
 char *name;
 float age;
 char *owner;
+/*
+ */
 };
 
 #endif
