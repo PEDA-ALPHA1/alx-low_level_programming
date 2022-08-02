728ca477be5990760bf711165dfa22f025c7528f
Peda-Alpha <peda4sure@gmail.com>
2022-08-01 18:22:39 -0700

    Create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,12 +1,12 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
-* new_dog - creates a new dog.
-* @name: pointer to a char for name of dog
-* @age: age of dog
-* @owner: pointer to a char for owner of dog
-* Return: pointer to a new dog of type dog_t
-**/
+ * * new_dog - creates a new dog.
+ * * @name: pointer to a char for name of dog
+ * * @age: age of dog
+ * * @owner: pointer to a char for owner of dog
+ * * Return: pointer to a new dog of type dog_t
+ * **/
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int nameLen, ownerLen, i;
@@ -28,8 +28,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (i = 0; i <= nameLen; i++)
 		d1->name[i] = name[i];
-		d1->age = age;
-		d1->owner = malloc(ownerLen * sizeof(d1->owner));
+	d1->age = age;
+	d1->owner = malloc(ownerLen * sizeof(d1->owner));
 	if (d1->owner == NULL)
 	{
 		free(d1->name);
