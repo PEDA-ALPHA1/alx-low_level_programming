60f7c6a8bfa1146eedb58a6e0b7de6473d735d72
Peda-Alpha <peda4sure@gmail.com>
2022-08-02 02:10:36 -0700

    Set date

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -9,6 +9,6 @@ void print_remaining_days(int month, int day, int year);
 
 void positive_or_negative(int i);
 
-int largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
