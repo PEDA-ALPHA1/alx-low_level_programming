3e97bb2711b0a8737e1b7373045df1ee75e40c03
Peda-Alpha <peda4sure@gmail.com>
2022-08-02 01:22:04 -0700

    Print the name of a file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Print the name of the file compiled
+ * @argc: Argument counter
+ * @argv: Argument vector
+ * Return: Always 0
+ * **/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
