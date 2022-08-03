23db237d531345c8c9b60299ed969bfd363f943a
Peda-Alpha <peda4sure@gmail.com>
2022-08-03 07:42:20 -0700

    Loop through an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,17 +1,18 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 /**
- *
- *
- *
+ *array_iterator - Function prototype
+ *@array: Pointer of type int
+ *@size: Size of array
+ *@action: Pointer to function
  * **/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int x;
+	unsigned long int x;
 
 	if (array != NULL && action != NULL)
 	for (x = 0; x < size; x++)
 	{
 		action(array[x]);
 	}
-}	
+}
