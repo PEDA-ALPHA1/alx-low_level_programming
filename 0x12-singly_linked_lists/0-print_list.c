395c849c024fcf5de1bd0e96947b8e755db014f3
PEDA-ALPHA1 <daep100@gmail.com>
2022-08-11 15:47:24 -0700

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - Function prototype
+ * @h: Argument of type pointer list_t
+ * return size_t
+ * Description: Prints all the elements of a list_t list
+ * **/
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[%d] (%s)\n", 0, "nil");
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		
+		h = h->next;
+		count++;
+	}
+	return (count);	
+}
