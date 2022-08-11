a2261802c00bedbfc9fd15b3f08ae627bbd74cbb
PEDA-ALPHA1 <daep100@gmail.com>
2022-08-11 16:39:17 -0700

    Fix requirement

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,28 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
-*print_list - print string and length of string for each element of 'h'
-*@h: pointer to first element of list
-*Return: length of list
-*/
+ * print_list - prints all elements of a list_t list
+ * @h: strings inputted
+ *
+ * Return: number of nodes
+ */
 size_t print_list(const list_t *h)
 {
-size_t size = 0;
-while (h)
-{
-if (h->str == NULL)
-{
-printf("[%u] (nil)\n", h->len);
-h = h->next;
-++size;
-continue;
-}
-printf("[%u] %s\n", h->len, h->str);
-h = h->next;
-++size;
-}
-return (size);
+	size_t count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[%d] (%s)\n", 0, "nil");
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
