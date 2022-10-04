d83cec92286fa8055dcfc89c989644cb78037783
PEDA-ALPHA1 <peda4sure@gmail.com>
2022-10-03 17:44:10 -0700

    Dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/*int _strlen(char *s);*/
+/**
+* _strncat - concatenate two strings
+* @dest: string
+* @src: string
+* @n: number of elements to concatenate in
+* Return: pointer to resulting `dest`
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int size1 = _strlen(dest);
+int size2 = _strlen(src);
+int x;
+int y = 0;
+if (n > size2)
+{
+n = size2;
+}
+for (x = size1; x < (size1 + n); x++)
+{
+dest[x] = src[y];
+y++;
+}
+if (n < size2)
+{
+dest[x] = '\0';
+}
+return (dest);
+}
+/**
+* _strlen - swap funcion
+* @s: Number that is going to be splited
+* Return: Return the last digit
+*/
+/*int _strlen(char *s)
+{
+int c = 0;
+while (s[c] != '\0')
+{
+c++;
+}
+return (c);
+}*/
